ccf17ee0cec74fe6042fe998a707a67d19e4ce6e
Vitalino Silva <vitalinosilva@gmail.com>
2023-05-13 21:24:27 +0100

    updated

--- app.vue
+++ app.vue
@@ -1,5 +1,10 @@
 <template>
-  <div>
-      <h1 class="text-red-500">Clone</h1>
+  <div :class="{dark: darkMode}">
+      <h1 class="text-red-500 dark:text-dim-500">Clone</h1>
   </div>
 </template>
+
+<script setup>
+const darkMode = ref(false);
+
+</script>
