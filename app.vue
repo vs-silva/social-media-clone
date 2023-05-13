45d842cfdb71976ed8e9312a5757c1a0896a11d3
Vitalino Silva <vitalinosilva@gmail.com>
2023-05-13 23:52:55 +0100

    working on the right sidebar

--- app.vue
+++ app.vue
@@ -8,15 +8,23 @@
               <div class="grid grid-cols-12 mx-auto sm:px-6 lg:max-w-7xl lg:px-8 lg:gap-5">
 
                   <!--Sidebar left-->
-                  <div class="hidden md:block xs-col-span-1 xl:col-span-2">
+                  <div class="hidden md:block xs:col-span-1 xl:col-span-2">
                       <div class="sticky top-0">
                           <sidebar-left />
                       </div>
                   </div>
 
-                  <!--Sidebar content-->
+                  <!--Main content-->
+                  <!--<main class="col-span-12 md:col-span-8 xl:col-span-6 bg-red-500">
+                      <h1>HERE</h1>
+                  </main> -->
 
                   <!--Sidebar right-->
+                  <div class="col-span-12 bg-blue-400 md:block md:col-span-3 xl:col-span-4">
+                      <div class="sticky top-0">
+                          <sidebar-right />
+                      </div>
+                  </div>
 
               </div>
 
---
+++ components/sidebar/right/index.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="flex flex-col">
+
+      <!-- preview card: What's happening -->
+      <sidebar-right-preview-card :title="`What's happening`">
+
+          <sidebar-right-preview-card-item>
+
+          </sidebar-right-preview-card-item>
+
+      </sidebar-right-preview-card>
+
+      <!-- preview card: Who to follow -->
+      <sidebar-right-preview-card :title="`Who to follow`"/>
+
+  </div>
+</template>
+
+<script setup lang="ts">
+
+</script>
+
+<style scoped>
+
+</style>
