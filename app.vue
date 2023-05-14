99d3c38d2f3f2240a3b9e0be79c635d1d5f1fa59
Vitalino Silva <vitalinosilva@gmail.com>
2023-05-14 01:06:39 +0100

    base layout completed

--- app.vue
+++ app.vue
@@ -15,8 +15,8 @@
                   </div>
 
                   <!--Main content-->
-                  <main class="col-span-12 md:col-span-8 xl:col-span-6 bg-red-500">
-                      <h1>HERE</h1>
+                  <main class="col-span-12 md:col-span-8 xl:col-span-6">
+                      <router-view />
                   </main>
 
                   <!--Sidebar right-->
---
+++ pages/index.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+      <main-section :title="`Home`" :loading="loading">
+          this is the home page!
+      </main-section>
+  </div>
+
+</template>
+
+<script setup lang="ts">
+const loading = ref(false);
+
+</script>
+
+<style scoped>
+
+</style>
