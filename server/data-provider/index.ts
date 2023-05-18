4bf627968ee597401d03451a9b1704145bce5bf1
Vitalino Silva <vitalinosilva@gmail.com>
2023-05-18 16:55:00 +0100

    updated

--- server/data-provider/index.ts
+++ server/data-provider/index.ts
@@ -1,11 +1,4 @@
 import { PrismaClient } from '@prisma/client';
-import Settings from "../settings";
 
-const prisma = new PrismaClient({
-    datasources: {
-        db: {
-            url: Settings.dbConnectionString as string
-        }
-    }
-});
+const prisma = new PrismaClient();
 export default prisma;
--- server/settings/index.ts
+++ server/settings/index.ts
@@ -1,5 +1,7 @@
+const config = useRuntimeConfig();
+
 export default {
-    dbConnectionString: process.env.DATABASE_URL,
-    accessTokenSecret: process.env.JWT_ACCESS_TOKEN_SECRET,
-    refreshTokenSecret: process.env.JWT_REFRESH_TOKEN_SECRET
+    dbConnectionString: config.databaseURL,
+    accessTokenSecret: config.jwtAccessToken,
+    refreshTokenSecret: config.jwtRefreshToken
 };
